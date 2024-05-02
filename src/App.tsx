c498d5976860d8508465b7f7e57b92557f1aa539
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-05-02 14:26:23 +0300

    add toggle button and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import AppRouter from './AppRouter';
+import MaterialUISwitch from './components/styles/Switch'; 
+import { ThemeProvider } from '@mui/material';
+import { createCustomTheme } from './theme/theme'; 
+import { GlobalStyle } from './components/styles/styles'; 
+import { Box } from '@mui/material'; 
 
-function App() {
-  
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(true); 
+
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const theme = createCustomTheme(darkMode);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = theme.palette.background.default;
+    document.body.style.color = theme.palette.text.primary;
+  }, [theme]);
+
   return (
-    <>
-      <div>
-        
-      </div>
-     
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Box sx={{ position: 'absolute', top: 0, right: 0, margin: '10px' }}> 
+        <MaterialUISwitch
+          checked={darkMode}
+          onChange={handleToggleDarkMode}
+          inputProps={{ 'aria-label': 'toggle dark mode' }}
+          color="secondary"
+          sx={{ mr: 1 }}
+        />
+      </Box>
+      <AppRouter />
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
