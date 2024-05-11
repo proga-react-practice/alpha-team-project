7a27eca031ef0155e5842b1607c337296b5bcaa1
Anna Tyshchenko <annati4nko@gmail.com>
2024-05-11 04:05:47 +0300

    drag and drop cards along x-axis

--- src/components/pages/CardsPage.tsx
+++ src/components/pages/CardsPage.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import { FormData as MusicFormData } from "./music";
 import { FormDataUser as UserFormData } from "./user";
 import { Grid } from "@mui/material";
-
+import { Reorder } from "framer-motion";
 
 interface CardsPageProps {
   musicData: MusicFormData[];
@@ -11,14 +11,30 @@ interface CardsPageProps {
 }
 
 const CardsPage: React.FC<CardsPageProps> = ({ musicData, userData }) => {
+  const [cards, setCard] = useState(musicData);
+
   return (
+    <Reorder.Group values={cards} onReorder={setCard} axis="x">
       <Grid container spacing={2}>
-        {musicData.map((data, index) => (
+        {cards.map((data, index) => (
           <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-            <Card key={index} data={data} dataUser={userData[index]} />
+            <Reorder.Item
+              value={data}
+              key={index}
+              whileDrag={{ scale: 1.1 }}
+              initial={{ opacity: 0, y: 30 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.3 },
+              }}
+            >
+              <Card key={index} data={data} dataUser={userData[index]} />
+            </Reorder.Item>
           </Grid>
         ))}
       </Grid>
+    </Reorder.Group>
   );
 };
 
