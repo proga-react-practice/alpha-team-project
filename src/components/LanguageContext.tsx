983056cea4ee7297e643f4af932a1c92972db83e
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-05-17 17:55:17 +0300

    add page language change

---
+++ src/components/LanguageContext.tsx
@@ -0,0 +1,40 @@
+
+import { createContext, useContext, useState, ReactNode } from "react";
+import enTranslations from './translation/en.json';
+import uaTranslations from './translation/ua.json';
+
+type Language = 'en' | 'ua';
+
+interface LanguageContextProps {
+  language: Language;
+  translations: any;
+  toggleLanguage: () => void;
+}
+
+const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
+
+export const LanguageProvider = ({ children }: { children: ReactNode }) => {
+  const [language, setLanguage] = useState<Language>('en');
+
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) => (prevLanguage === 'en' ? 'ua' : 'en'));
+  };
+
+  const translations = language === 'en' ? enTranslations : uaTranslations;
+
+  console.log("Current language:", language);
+
+  return (
+    <LanguageContext.Provider value={{ language, translations, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};
