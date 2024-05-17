ae64b0aa75aad00115a1bf3c8f871d2a00996ef1
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-05-17 19:54:37 +0300

    add switch button for languages

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import { MaterialUISwitch } from "./components/styled/Switch";
 import { ThemeProvider } from "@mui/material";
 import { createCustomTheme } from "./theme/theme";
 import { GlobalStyle } from "./components/styled/styles";
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import { LanguageProvider, useLanguage } from "./components/LanguageContext"; 
+
+import uaFlag from './img/ua.png';
+import enFlag from './img/en.png';
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState<boolean>(true);
+  const { language, toggleLanguage } = useLanguage();
 
   const handleToggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -24,27 +29,43 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Box
-  sx={{
-    position: "fixed",
-    top: "3px", 
-    right: "20px",
-    zIndex: 2,
-    padding: "10px",
-  }}
->
-  <MaterialUISwitch
-    checked={darkMode}
-    onChange={handleToggleDarkMode}
-    inputProps={{ "aria-label": "toggle dark mode" }}
-    color="secondary"
-    sx={{ marginTop: "10px" }}
-  />
-</Box>
-
+        sx={{
+          position: "fixed",
+          top: "2px",
+          right: "20px",
+          zIndex: 2,
+          padding: "10px",
+          display: "flex",
+          alignItems: "center",
+          gap: "10px"
+        }}
+      >
+        <IconButton
+          onClick={toggleLanguage} 
+          aria-label="toggle language"
+          sx={{ marginTop: '5px' }}  
+        >
+          <img src={language === 'en' ? enFlag : uaFlag} alt="language flag" style={{ width: '35px', height: '35px' }} />
+        </IconButton>
+        <MaterialUISwitch
+          checked={darkMode}
+          onChange={handleToggleDarkMode}
+          inputProps={{ "aria-label": "toggle dark mode" }}
+          color="secondary"
+          sx={{ marginTop: "5px" }}
+        />
+      </Box>
       <AppRouter />
     </ThemeProvider>
   );
 };
 
-export default App;
+const Root: React.FC = () => {
+  return (
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  );
+};
 
+export default Root;
