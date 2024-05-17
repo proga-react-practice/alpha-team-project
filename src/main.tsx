96000c0298e29929e236caf50dd9b0074204203b
Anna Tyshchenko <annati4nko@gmail.com>
2024-05-17 20:12:06 +0300

    language provider/ img fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import {
   MusicProvider,
   UserProvider,
 } from "./components/pages/DataContext.tsx";
+import { LanguageProvider } from "./components/LanguageContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MusicProvider>
     <UserProvider>
+      <LanguageProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
+      </LanguageProvider>
     </UserProvider>
   </MusicProvider>
 );
