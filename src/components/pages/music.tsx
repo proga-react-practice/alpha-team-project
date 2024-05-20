4610571059c67e4330dd4a7c066477bdf41a9e6f
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-05-20 12:28:14 +0300

    dynamic errors

--- src/components/pages/music.tsx
+++ src/components/pages/music.tsx
@@ -12,7 +12,6 @@ import {
   FormLabel,
   FormHelperText,
 } from "@mui/material";
-
 import {
   FormContainer,
   CustomButton,
@@ -58,6 +57,7 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
     register,
     handleSubmit,
     reset,
+    trigger,
     formState: { errors },
   } = useForm<FormData>({ defaultValues: { id } });
 
@@ -71,6 +71,7 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
     setFormData((prevData) => [...prevData, data]);
     navigate("/playlist");
   };
+
   const handleReset = () => {
     setId(new Date().getTime().toString());
     reset();
@@ -103,18 +104,18 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
           <TextField
             label={translations.form.musicLabel}
             {...register("name", {
-              required: "Name is required",
+              required:  translations.musicErrors.name.required,
               minLength: {
                 value: 2,
-                message: "Name must be at least 2 character long",
+                message: translations.musicErrors.name.minLength,
               },
               maxLength: {
                 value: 20,
-                message: "Name must be less than 20 character long",
+                message: translations.musicErrors.name.maxLength,
               },
               validate: (value) =>
                 value.charAt(0) === value.charAt(0).toUpperCase() ||
-                "Name should start with a capital letter",
+                translations.musicErrors.name.validate,
             })}
             variant="outlined"
             margin="normal"
@@ -122,13 +123,18 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
             sx={{ marginBottom: 1.5 }}
             error={!!errors.name}
             helperText={errors.name?.message}
+            onBlur={() => trigger("name")}
+            onChange={(e) => {
+              register("name").onChange(e);
+              trigger("name");
+            }}
           />
 
           <Controller
             name="genre"
             control={control}
             defaultValue=""
-            rules={{ required: { value: true, message: "Genre is required" } }}
+            rules={{ required: { value: true, message: translations.musicErrors.genre.validate} }}
             render={({ field }) => (
               <FormControl
                 variant="outlined"
@@ -136,7 +142,15 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
                 sx={{ marginBottom: 1.5 }}
               >
                 <InputLabel>{translations.form.musicGenre}</InputLabel>
-                <Select {...field} label={translations.form.musicGenre}>
+                <Select
+                  {...field}
+                  label={translations.form.musicGenre}
+                  onBlur={() => trigger("genre")}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    trigger("genre");
+                  }}
+                >
                   {Object.values(Genre).map((genre) => (
                     <MenuItem key={genre} value={genre}>
                       {translations.enums.Genre[genre]}
@@ -149,41 +163,47 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
               </FormControl>
             )}
           />
-          <FormControl variant="outlined" fullWidth sx={{ marginBottom: 1.5 }}>
+
+          <FormControl variant="outlined" fullWidth sx={{ marginBottom: 1.5, marginTop: -2 }}>
             <TextField
               label={translations.form.artistLabel}
               {...register("artist", {
-                required: "Artist is required",
+                required: translations.musicErrors.artist.required,
                 minLength: {
                   value: 2,
-                  message: "Artist must be at least 2 character long",
+                  message: translations.musicErrors.artist.minLength,
                 },
                 maxLength: {
                   value: 20,
-                  message: "Artist must be less than 20 character long",
+                  message: translations.musicErrors.artist.maxLength,
                 },
                 validate: (value) =>
                   value.charAt(0) === value.charAt(0).toUpperCase() ||
-                  "Artist should start with a capital letter",
+                  translations.musicErrors.artist.validate,
               })}
               variant="outlined"
               margin="normal"
               fullWidth
               error={!!errors.artist}
               helperText={errors.artist?.message}
+              onBlur={() => trigger("artist")}
+              onChange={(e) => {
+                register("artist").onChange(e);
+                trigger("artist");
+              }}
             />
           </FormControl>
 
-          <FormControl variant="outlined" fullWidth sx={{ marginBottom: 1.5 }}>
+          <FormControl variant="outlined" fullWidth sx={{ marginBottom: 1.5, marginTop: -1 }}>
             <FormLabel htmlFor="releasedOn">
               {translations.form.dateLabel}
             </FormLabel>
             <TextField
               type="date"
               {...register("releasedOn", {
-                required: "Date is required",
-                min: { value: minDate, message: "Enter a date after 1980" },
-                max: { value: today, message: "Enter a date before today" },
+                required: translations.musicErrors.releasedOn.required,
+                min: { value: minDate, message: translations.musicErrors.releasedOn.min },
+                max: { value: today, message: translations.musicErrors.releasedOn.max},
               })}
               variant="outlined"
               InputLabelProps={{
@@ -192,8 +212,14 @@ const MusicForm: React.FC<MusicFormProps> = ({ onSubmit }) => {
               size="small"
               error={!!errors.releasedOn}
               helperText={errors.releasedOn?.message}
+              onBlur={() => trigger("releasedOn")}
+              onChange={(e) => {
+                register("releasedOn").onChange(e);
+                trigger("releasedOn");
+              }}
             />
           </FormControl>
+
           <CustomButton type="submit" variant="contained">
             {translations.form.submitButton}
           </CustomButton>
