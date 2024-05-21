08d3d1c4652dc28cf6d10ee7d2b6d9a449c376ca
Anna Tyshchenko <annati4nko@gmail.com>
2024-05-21 15:50:42 +0300

    timeline moved to separate component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,5 @@
-import { Box, Typography, useTheme, Grow, useMediaQuery } from "@mui/material";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineDot from "@mui/lab/TimelineDot";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Timeline from "./Timeline";
 import violet from "../../img/violet.svg";
 import green from "../../img/green.svg";
 import { useLanguage } from "../LanguageContext";
@@ -83,72 +78,7 @@ function OutlinedTimeline() {
   const { translations } = useLanguage();
   return (
     <Box>
-      <Timeline position="alternate" sx={{ width: "100%" }}>
-        <Grow in={true} timeout={1000}>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography variant="body1" sx={{ textAlign: "left" }}>
-                {translations.home.timeline1}
-              </Typography>
-              <Typography variant="body2" sx={{ textAlign: "left" }}>
-                {translations.home.description1}
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Grow>
-        <Grow in={true} timeout={1500}>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" color="primary" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography variant="body1" sx={{ textAlign: "left" }}>
-                {translations.home.timeline2}
-              </Typography>
-              <Typography variant="body2" sx={{ textAlign: "left" }}>
-                {translations.home.description2}
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Grow>
-        <Grow in={true} timeout={2000}>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" color="secondary" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography variant="body1" sx={{ textAlign: "left" }}>
-                {translations.home.timeline3}
-              </Typography>
-              <Typography variant="body2" sx={{ textAlign: "left" }}>
-                {translations.home.description3}
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Grow>
-        <Grow in={true} timeout={2500}>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography variant="body1" sx={{ textAlign: "left" }}>
-                {translations.home.timeline4}
-              </Typography>
-              <Typography variant="body2" sx={{ textAlign: "left" }}>
-                {translations.home.description4}
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Grow>
-      </Timeline>
-
+      <Timeline />
       <Box
         sx={{
           textAlign: "center",
