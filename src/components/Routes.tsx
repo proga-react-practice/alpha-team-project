fd435311583299e8455e3e209ce76e130dd8a765
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-05-21 15:55:23 +0300

    use hardcoded route

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -3,23 +3,32 @@ import { useTheme } from "@mui/material/styles";
 import { StyledLink } from "./styled/styles";
 import { useLanguage } from "./LanguageContext";
 
+export const routes = {
+  home: "/home",
+  user: "/user",
+  music: "/music",
+  cards: "/cards",
+  favorites: "/favorites",
+  playlist: "/playlist",
+};
+
 const Routes: React.FC<{ onClick: () => void }> = ({ onClick }) => {
   const theme = useTheme();
   const { translations } = useLanguage();
   const linkStyle = { color: theme.palette.mode === "dark" ? "#ffffff" : "#000000" };
 
-  const routes = [
-    { path: "/home", label: translations.routes.homepage },
-    { path: "/user", label: translations.routes.userform },
-    { path: "/music", label: translations.routes.musicform },
-    { path: "/cards", label: translations.routes.cards },
-    { path: "/favorites", label: translations.routes.favorites },
-    { path: "/playlist", label: translations.routes.playlist}
+  const routeList = [
+    { path: routes.home, label: translations.routes.homepage },
+    { path: routes.user, label: translations.routes.userform },
+    { path: routes.music, label: translations.routes.musicform },
+    { path: routes.cards, label: translations.routes.cards },
+    { path: routes.favorites, label: translations.routes.favorites },
+    { path: routes.playlist, label: translations.routes.playlist }
   ];
 
   return (
     <>
-      {routes.map(({ path, label }) => (
+      {routeList.map(({ path, label }) => (
         <StyledLink key={path} to={path} sx={linkStyle} onClick={onClick}>
           {label}
         </StyledLink>
